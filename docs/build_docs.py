fd08154e4ccb242d17ff45feab09a732daf338f7
Tony Lownds <tony@lownds.com>
2014-09-21 13:50:08 -0700

    Docs now push to gh-pages

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -13,4 +13,4 @@ except:
     import pdb, traceback
     traceback.print_exc()
     pdb.post_mortem()
-open('index.html', 'w').write(html.encode('utf-8'))
+open('../gh-pages/index.html', 'w').write(html.encode('utf-8'))
